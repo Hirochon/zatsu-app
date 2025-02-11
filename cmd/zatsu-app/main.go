2ad67ccceab355abdcf9f8a95ecb2bf48cc881a0
Hirochon <heacet43@gmail.com>
2025-02-11 13:44:36 +0900

    Add initial Go project structure with basic HTTP server

---
+++ cmd/zatsu-app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "こんにちは世界!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module zatsu-app
+
+go 1.23.6
